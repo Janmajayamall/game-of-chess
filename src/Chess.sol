d85526a39f86338fcc83ca012e5b66f6873ad2d2
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-12-17 07:27:52 +0800

    init states

--- src/Chess.sol
+++ src/Chess.sol
@@ -2,9 +2,181 @@
 
 pragma solidity ^0.8.0;
 
-contract Chess {
-    constructor() {
 
-        
+import "ds-test/test.sol";
+
+contract Chess is DSTest {
+
+    // 12 boards
+
+    enum Piece { 
+        p, n, b, r, q, k, P, N, B, R, Q, K
+    }
+
+    // uint256[12] bitboards = [
+    //     // black pos
+    //     65280,
+    //     66,
+    //     36,
+    //     1,
+    //     8,
+    //     16,
+    //     // white pos
+    //     71776119061217280,
+    //     4755801206503243776,
+    //     1297036692682702848,
+    //     36028797018963968,
+    //     576460752303423488,
+    //     1152921504606846976
+    // ];
+
+    function test_isMoveValid() external {
+        // accept a move, extract source and target
+        uint64[12] memory bitboards =  [
+            // black pos
+            65280,
+            66,
+            36,
+            129,
+            8,
+            16,
+            // white pos
+            71776119061217280,
+            4755801206503243776,
+            2594073385365405696,
+            9295429630892703744,
+            576460752303423488,
+            1152921504606846976
+        ];
+
+        // black board
+        uint64 blackBoard = bitboards[uint(Piece.p)];
+        blackBoard |= bitboards[uint(Piece.n)];
+        blackBoard |= bitboards[uint(Piece.b)];
+        blackBoard |= bitboards[uint(Piece.r)];
+        blackBoard |= bitboards[uint(Piece.q)];
+        blackBoard |= bitboards[uint(Piece.k)];
+
+        // black board
+        uint64 whiteBoard = bitboards[uint(Piece.P)];
+        whiteBoard |= bitboards[uint(Piece.N)];
+        whiteBoard |= bitboards[uint(Piece.B)];
+        whiteBoard |= bitboards[uint(Piece.R)];
+        whiteBoard |= bitboards[uint(Piece.Q)];
+        whiteBoard |= bitboards[uint(Piece.K)];
+
+        assertTrue(false);
+
+        emit log_uint(blackBoard);
+        emit log_uint(whiteBoard);
     }
 }
+
+/**
+
+    Block pieces 
+
+    p ()
+    0 0 0 0 0 0 0 0 
+    1 1 1 1 1 1 1 1 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+
+    n
+    0 1 0 0 0 0 1 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0
+
+    r
+    1 0 0 0 0 0 0 1
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+
+    q
+    0 0 0 1 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0
+
+    k
+    0 0 0 0 1 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+
+    White pieces 
+    
+    P
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    1 1 1 1 1 1 1 1 
+    0 0 0 0 0 0 0 0 
+
+    N
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 1 0 0 0 0 1 0
+
+    R
+    0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    1 0 0 0 0 0 0 1
+
+    Q
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 1 0 0 0 0
+
+    K
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 
+    0 0 0 0 1 0 0 0 
+
+ */
+    
