bfa7b7aafeb6d542f180b6199f5ec8378290c500
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-12-25 13:56:56 +0800

    removed string parser

--- src/libraries/GameHelpers.sol
+++ src/libraries/GameHelpers.sol
@@ -11,40 +11,7 @@ library GameHelpers {
     using String for string;
     using Uint for uint;
 
-    function parseBitboardsToString(uint64[12] memory bitboards) internal pure returns (string memory) {
-        uint[] memory boardMap = new uint[](64);
-
-        // make every index 12 for overlapping indentification
-        for (uint256 index = 0; index < 64; index++) {
-            boardMap[index] = 12;
-        }
-
-        for (uint256 pIndex = 0; pIndex < 12; pIndex++) {
-            uint64 board = bitboards[pIndex];
-            for (uint256 index = 0; index < 64; index++) {
-                if (board & (1 << index) != 0){
-                    require(boardMap[index] == 12, "Invalid board");
-                    boardMap[index] = pIndex;
-                }
-            }
-        }
-
-        string memory boardString;
-        // convert board map string
-        for (uint256 index = 0; index < 64; index++) {
-            if (index % 8 == 0 && index != 0){
-                boardString = boardString.append(string(" | "));
-            }
-
-            boardString = boardString.append(string(" "));
-            boardString = boardString.append(boardMap[index].toString());
-            boardString = boardString.append(string(" "));
-        }
-
-        return boardString;
-    }
-
-    function parseUintToPieceChar(uint val) internal returns (string memory ){
+    function parseUintToPieceChar(uint val) internal pure returns (string memory ){
         require(val < 12, "Invalid Val");
         IGocDataTypes.Piece piece = IGocDataTypes.Piece(val);
         if (IGocDataTypes.Piece.p == piece){
@@ -85,7 +52,7 @@ library GameHelpers {
         }
     }
 
-    function parseGameStateToFenString(IGocDataTypes.GameState memory gameState) internal returns (string memory fen){
+    function parseGameStateToFenString(IGocDataTypes.GameState memory gameState) internal pure returns (string memory fen){
         uint[] memory boardMap = new uint[](64);
 
         // make every index 12 for overlapping indentification
